f5e2960523800ea16c9d4798820fc99a3a4d61f2
Thiago Martins <devthiagomartins@gmail.com>
2021-12-02 13:44:52 -0300

    Metodo PUT para tecnico implementado

--- src/main/java/br/com/thiago/helpdesk/services/TecnicoService.java
+++ src/main/java/br/com/thiago/helpdesk/services/TecnicoService.java
@@ -3,6 +3,8 @@ package br.com.thiago.helpdesk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,50 +19,56 @@ import br.com.thiago.helpdesk.services.exceptions.ObjectNotFoundException;
 @Service
 public class TecnicoService {
 
-	@Autowired 
+	@Autowired
 	private TecnicoRepository repository;
-	
+
 	@Autowired
 	private PessoaRepository pessoaRepository;
-	
+
 	public Tecnico findById(Integer id) {
-		
+
 		Optional<Tecnico> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id));
 	}
 
 	public List<Tecnico> findAll() {
-		
+
 		return repository.findAll();
-		
+
 	}
 
 	public Tecnico create(TecnicoDTO objDTO) {
-		
+
 		objDTO.setId(null);
 		validaPorCpfEEmail(objDTO);
 		Tecnico newObj = new Tecnico(objDTO);
 		return repository.save(newObj);
 	}
 
+	public Tecnico update(Integer id, @Valid TecnicoDTO objDto) {
+
+		objDto.setId(id);
+		Tecnico oldObj = findById(id);
+		validaPorCpfEEmail(objDto);
+		oldObj = new Tecnico(objDto);
+		
+		return repository.save(oldObj);
+	}
+
 	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
-		
+
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
-		
-		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
 			throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
 		}
-		
+
 		obj = pessoaRepository.findByEmail(objDTO.getEmail());
 
-		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
+		if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
 			throw new DataIntegrityViolationException("Email já cadastrado no sistema!");
 		}
-		
-		
-		
+
 	}
-	
-	
-	
+
 }
