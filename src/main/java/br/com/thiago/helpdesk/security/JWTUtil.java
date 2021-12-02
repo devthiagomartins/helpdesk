39aa95c08e569d03b86d5760ac2aa7bcc7f05a1b
Thiago Martins <devthiagomartins@gmail.com>
2021-12-02 18:15:08 -0300

    Autorizacao com tokens JWT

--- src/main/java/br/com/thiago/helpdesk/security/JWTUtil.java
+++ src/main/java/br/com/thiago/helpdesk/security/JWTUtil.java
@@ -5,24 +5,56 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
 public class JWTUtil {
 
-	@Value(value = "${jwt.expiration}")//3min pega do application.properties
+	@Value(value = "${jwt.expiration}") // 3min pega do application.properties
 	private Long expiration;
-	
-	@Value(value = "${jwt.secret}")//3min
+
+	@Value(value = "${jwt.secret}") // 3min
 	private String secret;
-	
+
 	public String generateToken(String email) {
-		return Jwts.builder()
-				.setSubject(email)
-				.setExpiration(new Date(System.currentTimeMillis() + expiration))
-				.signWith(SignatureAlgorithm.HS512, secret.getBytes())
-				.compact();
+		return Jwts.builder().setSubject(email).setExpiration(new Date(System.currentTimeMillis() + expiration))
+				.signWith(SignatureAlgorithm.HS512, secret.getBytes()).compact();
 	}
-	
+
+	public boolean tokenValido(String token) {
+		Claims claims = getClaims(token);
+		if (claims != null) {
+			String username = claims.getSubject();
+			Date expirationDate = claims.getExpiration();
+			Date now = new Date(System.currentTimeMillis());
+
+			if (username != null && expirationDate != null && now.before(expirationDate)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private Claims getClaims(String token) {
+		try {
+
+			return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
+
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	public String geUsername(String token) {
+		Claims claims = getClaims(token);
+
+		if (claims != null) {
+			return claims.getSubject();
+		}
+		return null;
+	}
+
 }
