1bfc80166dea6423eba7fb82834e986ed8257dc8
Thiago Martins <devthiagomartins@gmail.com>
2021-12-02 10:51:58 -0300

    alteracao na carga de dados e busca por tecnico resource

--- src/main/java/br/com/thiago/helpdesk/resources/TecnicoResource.java
+++ src/main/java/br/com/thiago/helpdesk/resources/TecnicoResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.thiago.helpdesk.domain.Tecnico;
+import br.com.thiago.helpdesk.domain.dtos.TecnicoDTO;
 import br.com.thiago.helpdesk.services.TecnicoService;
 
 @RestController
@@ -18,10 +19,10 @@ public class TecnicoResource {
 	private TecnicoService service;
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id){
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
 		
-		Tecnico obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
+		Tecnico obj = service.findById(id); 
+		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 		
 	}
 
