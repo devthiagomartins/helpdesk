63a427c0f25b042bc604714dbe6170167ccaa671
Thiago Martins Reis <devthiagomartins@gmail.com>
2023-05-18 21:43:14 -0300

    mapeamento de classes e objetos com anotacoes

--- src/main/java/com/thiago/helpdesk/domain/Cliente.java
+++ src/main/java/com/thiago/helpdesk/domain/Cliente.java
@@ -1,18 +1,30 @@
 package com.thiago.helpdesk.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Cliente extends Pessoa {
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.thiago.helpdesk.domain.enums.Perfil;
+
+@Entity
+public class Cliente extends Pessoa implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 	
+	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<Chamado>();
 
 	public Cliente() {
 		super();
+		adicionarPerfil(Perfil.CLIENTE);	
 	}
 
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		adicionarPerfil(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
--- src/main/java/com/thiago/helpdesk/domain/Tecnico.java
+++ src/main/java/com/thiago/helpdesk/domain/Tecnico.java
@@ -1,18 +1,30 @@
 package com.thiago.helpdesk.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Tecnico extends Pessoa {
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 
+import com.thiago.helpdesk.domain.enums.Perfil;
+
+@Entity
+public class Tecnico extends Pessoa implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<Chamado>();
 
 	public Tecnico() {
 		super();
+		adicionarPerfil(Perfil.CLIENTE);
 	}
 
 	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		adicionarPerfil(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
