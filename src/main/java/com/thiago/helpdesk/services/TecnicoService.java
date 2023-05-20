c91f72ec2856d500a63d0659548feb866ad2373d
Thiago Martins Reis <devthiagomartins@gmail.com>
2023-05-20 11:19:13 -0300

    Tecnico - update

--- src/main/java/com/thiago/helpdesk/services/TecnicoService.java
+++ src/main/java/com/thiago/helpdesk/services/TecnicoService.java
@@ -3,6 +3,8 @@ package com.thiago.helpdesk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -38,6 +40,14 @@ public class TecnicoService {
 		return tecnicoRepository.save(newObj);
 	}
 
+	public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
+		objDTO.setId(id);
+		Tecnico oldObj = findById(id);
+		validarPorCpfEmail(objDTO);
+		oldObj = new Tecnico(objDTO);
+		return tecnicoRepository.save(oldObj);
+	}
+	
 	private void validarPorCpfEmail(TecnicoDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
 		if(obj.isPresent() && obj.get().getId() != objDTO.getId()) {
@@ -49,4 +59,6 @@ public class TecnicoService {
 			throw new DataIntegrityViolationException("Email já cadastrado no sistema");
 		}
 	}
+
+
 }
